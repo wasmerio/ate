cb127dfd25bc0f2dbf95fa5358ba6e815a653d0f
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2019-04-25 07:58:32 +0200

    Now including the full pom in the repo

--- compile.sh
+++ compile.sh
@@ -13,5 +13,5 @@ cd target/lib; zip -r ../libs.zip *
 cd ../..
 
 mkdir -p bin-lib
-mvn install:install-file -Dfile=target/ate-0.1.jar -DgroupId=com.tokera -DartifactId=ate -Dversion=0.1 -Dpackaging=jar -DlocalRepositoryPath=bin-lib
+mvn install:install-file -Dfile=target/ate-0.1.jar -DgroupId=com.tokera -DartifactId=ate -Dversion=0.1 -Dpackaging=jar -DlocalRepositoryPath=bin-lib -DpomFile=pom.xml
 zip -r repo.zip bin-lib
