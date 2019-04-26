0dee93e6ec78d7d27ce0420a383ac470044f642e
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2019-04-26 23:33:13 +0200

    Now the ATE library is a bit more version aware

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-[ -e target/ate-0.1.jar ] && rm -f target/ate-0.1.jar || true
+rm -f target/*.jar || true
 [ -e repo.zip ] && rm -f repo.zip || true
 [ -e target/libs.zip ] && rm -f target/libs.zip || true
 
@@ -8,10 +8,15 @@ export MAVEN_OPTS="-Dmaven.repo.local=/maven"
 
 mvn -T 2C compile test package
 
-echo "Hash: $(cat target/ate-0.1.jar | md5sum)"
+JAR=$(basename target/*.jar)
+[ -z "$JAR" ] && echo "No JAR file exists" && exit 1
+VERSION=$(echo $JAR | cut -d "-" -f2 | sed 's|\.jar||g')
+[ -z "$VERSION" ] && echo "Version could not be determined" && exit 1
+
+echo "Hash: $(cat target/$JAR | md5sum)"
 cd target/lib; zip -r ../libs.zip *
 cd ../..
 
 mkdir -p bin-lib
-mvn install:install-file -Dfile=target/ate-0.2.jar -DgroupId=com.tokera -DartifactId=ate -Dversion=0.2 -Dpackaging=jar -DlocalRepositoryPath=bin-lib -DpomFile=pom.xml
+mvn install:install-file -Dfile=target/$JAR -DgroupId=com.tokera -DartifactId=ate -Dversion=$VERSION -Dpackaging=jar -DlocalRepositoryPath=bin-lib -DpomFile=pom.xml
 zip -r repo.zip bin-lib
