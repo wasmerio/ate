a0216096e17dc3c08cab8e639ebb96f2a486cc4b
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2019-04-28 10:48:14 +0200

    Switched to a different build container for tokapi

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ rmdir .m2 2>/dev/null || true
 # Copy all the maven files we already have over to the maven folder
 if [ ! -d .m2 ]; then
   mkdir -p .m2
-  docker run -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd)/.m2:/maven-local tokera/buildj1:latest cp -a -f -r /maven/. /maven-local/
+  docker run -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd)/.m2:/maven-local tokera/buildj:latest cp -a -f -r /maven/. /maven-local/
 fi
 
 # Execute the build (using the maven cache)
-docker run -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/build -v $(pwd)/.m2:/maven -w /build tokera/buildj1:latest make inside
+docker run -u $(id -u ${USER}):$(id -g ${USER}) -v $(pwd):/build -v $(pwd)/.m2:/maven -w /build tokera/buildj:latest make inside
 
 JAR=$(basename target/*.jar)
 [ -z "$JAR" ] && echo "No JAR file exists" && exit 1
