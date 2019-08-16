7e980f06b8ae11e74697007995b84e1140474aea
john <johnathan.sharratt@gmail.com>
2019-08-16 23:38:01 +0200

    Fixed the unit tests

--- src/test/java/com/tokera/ate/test/rest/BasicIntegrationTests.java
+++ src/test/java/com/tokera/ate/test/rest/BasicIntegrationTests.java
@@ -74,8 +74,6 @@ public class BasicIntegrationTests {
         NewAccountDto newDetails = new NewAccountDto();
         newDetails.setEmail("test@mycompany.org");
 
-        session.setSession("sdjbabsdf");
-
         MyAccount ret = session.restPut("/acc/register", Entity.entity(newDetails, MediaType.APPLICATION_JSON_TYPE), MyAccount.class);
         this.session.appendToPrefixForFs(ret.id + "/");
         this.session.appendToPrefixForRest(ret.id + "/");
