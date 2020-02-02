de62af30a28dce3b8880abf9ce63329572e3d2a2
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2020-02-02 08:26:59 +0100

    Ridiculously secure now uses AES 256 instead of AES 512 which doesnt exist anyway

--- src/main/java/com/tokera/ate/SecurityLevel.java
+++ src/main/java/com/tokera/ate/SecurityLevel.java
@@ -52,7 +52,7 @@ public class SecurityLevel {
         this.tokenExpiresMins = tokenExpiresMins;
     }
 
-    public static SecurityLevel RidiculouslySecure = new SecurityLevel(256, 512, 512, true, Lists.newArrayList(KeyType.qtesla, KeyType.rainbow), Lists.newArrayList(KeyType.aes, KeyType.ntru), true, true, 1);
+    public static SecurityLevel RidiculouslySecure = new SecurityLevel(256, 512, 256, true, Lists.newArrayList(KeyType.qtesla, KeyType.rainbow), Lists.newArrayList(KeyType.aes, KeyType.ntru), true, true, 1);
     public static SecurityLevel VeryHighlySecure = new SecurityLevel(256, 256, 256, true, Lists.newArrayList(KeyType.qtesla), Lists.newArrayList(KeyType.aes), true, true, 5);
     public static SecurityLevel HighlySecure = new SecurityLevel(192, 192, 192, false, Lists.newArrayList(KeyType.qtesla), Lists.newArrayList(KeyType.aes), true, true, 20);
     public static SecurityLevel ModeratelySecure = new SecurityLevel(128, 128, 128, false, Lists.newArrayList(KeyType.qtesla), Lists.newArrayList(KeyType.aes), false, true, 0);
