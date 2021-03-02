19617c1ae5db312ba69a521bcf6ef836568360e9
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2021-03-02 18:11:41 +0100

    Split out the error file so that its easier to manage the library

---
+++ src/error.rs
@@ -0,0 +1,13 @@
+#[derive(PartialEq, PartialOrd, Eq, Ord, Debug, Hash)]
+pub enum CryptoError {
+    NoIvPresent,    
+}
+
+impl From<CryptoError>
+for std::io::Error {
+    fn from(error: CryptoError) -> Self {
+        match error {
+            CryptoError::NoIvPresent => std::io::Error::new(std::io::ErrorKind::Other, "The metadata does not have IV component present")
+        }
+    }
+}
