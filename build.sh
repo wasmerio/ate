b19b3e9413cb4be14eac0d7ed15c9c9d69862c15
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2021-04-05 17:19:15 +0200

    Added some bash scripts for building and installing binaries

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+cargo build --release --bin atedb
+cargo build --release --bin atefs
+
---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+cp -f target/release/atedb /usr/bin/
+cp -f target/release/atefs /usr/bin/
