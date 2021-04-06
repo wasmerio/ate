8732562127b1e68989aae5ad5693bd9e9d71a87d
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2021-04-07 00:11:36 +0200

    No longer always installing packages

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-apt install cargo make pkg-config libfuse-dev libfuse3-dev openssl libssl-dev
+#sudo apt install cargo make pkg-config libfuse-dev libfuse3-dev openssl libssl-dev
 cargo build --release --bin atedb
 cargo build --release --bin atefs
 
