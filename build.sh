c118ba98c04e720e74c5cde44f5b85d7e24c7e15
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2021-04-12 21:56:16 +0200

    Now building the authentication server

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 #sudo apt install cargo make pkg-config libfuse-dev libfuse3-dev openssl libssl-dev
 cargo build --release --bin atedb
 cargo build --release --bin atefs
-
+cargo build --release --example auth-server
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
 cp -f target/release/atedb /usr/bin/
 cp -f target/release/atefs /usr/bin/
+cp -f target/release/examples/auth-server /usr/bin
