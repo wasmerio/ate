760a0ff67f784323c67514a895c41dced3b38034
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2021-04-17 23:09:48 +0200

    Implemeneted DNS server retry logic that will reconnect

--- lib/src/mesh/registry.rs
+++ lib/src/mesh/registry.rs
@@ -44,12 +44,61 @@ use trust_dns_proto::{
 
 enum DnsClient
 {
-    Dns(MemoizeClientHandle<AsyncClient>),
-    DnsSec(DnssecDnsHandle<MemoizeClientHandle<AsyncClient>>)
+    Dns {
+        cfg: ConfAte,
+        client: MemoizeClientHandle<AsyncClient>
+    },
+    DnsSec {
+        cfg: ConfAte,
+        client: DnssecDnsHandle<MemoizeClientHandle<AsyncClient>>
+    }
 }
 
 impl DnsClient
 {
+    async fn connect(cfg: &ConfAte) -> DnsClient
+    {
+        debug!("using DNS server: {}", cfg.dns_server);
+        let addr: SocketAddr = (cfg.dns_server.clone(), 53).to_socket_addrs().unwrap().next().unwrap();
+        
+        let (stream, sender)
+            = TcpClientStream::<AsyncIoTokioAsStd<TokioTcpStream>>::new(addr);
+        let client
+            = AsyncClient::new(stream, sender, None);
+        let (client, bg)
+            = client.await.expect("client failed to connect");
+        tokio::spawn(bg);
+
+        let client = MemoizeClientHandle::new(client);
+
+        match cfg.dns_sec {
+            false => {
+                debug!("configured for DNSSec");
+                DnsClient::Dns {
+                    cfg: cfg.clone(),
+                    client
+                }
+            },
+            true => {
+                debug!("configured for plain DNS");
+                DnsClient::DnsSec {
+                    cfg: cfg.clone(),
+                    client: DnssecDnsHandle::new(client.clone())
+                }
+            }
+        }
+    }
+
+    async fn reconnect(&mut self)
+    {
+        let cfg = match self {
+            DnsClient::Dns { cfg, client: _} => cfg.clone(),
+            DnsClient::DnsSec { cfg, client: _} => cfg.clone()
+        };
+
+        *self = DnsClient::connect(&cfg).await;
+    }
+
     async fn query(
         &mut self,
         name: Name,
@@ -57,9 +106,23 @@ impl DnsClient
         query_type: RecordType,
     ) -> Result<DnsResponse, ClientError>
     {
-        match self {
-            DnsClient::Dns(c) => c.query(name, query_class, query_type).await,
-            DnsClient::DnsSec(c) => c.query(name, query_class, query_type).await,
+        let ret = {
+            match self {
+                DnsClient::Dns{cfg: _, client: c} => c.query(name.clone(), query_class, query_type).await,
+                DnsClient::DnsSec{cfg: _, client: c} => c.query(name.clone(), query_class, query_type).await,
+            }
+        };
+
+        match ret {
+            Ok(a) => Ok(a),
+            Err(_) => {
+                self.reconnect().await;
+
+                match self {
+                    DnsClient::Dns{cfg: _, client: c} => c.query(name, query_class, query_type).await,
+                    DnsClient::DnsSec{cfg: _, client: c} => c.query(name, query_class, query_type).await,
+                }
+            }
         }
     }
 }
@@ -76,28 +139,7 @@ impl Registry
 {
     pub async fn new(cfg_ate: &ConfAte, temporal: bool) -> Arc<Registry>
     {
-        debug!("using DNS server: {}", cfg_ate.dns_server);
-        let addr: SocketAddr = (cfg_ate.dns_server.clone(), 53).to_socket_addrs().unwrap().next().unwrap();
-        
-        let (stream, sender)
-            = TcpClientStream::<AsyncIoTokioAsStd<TokioTcpStream>>::new(addr);
-        let client
-            = AsyncClient::new(stream, sender, None);
-        let (client, bg)
-            = client.await.expect("client failed to connect");
-        tokio::spawn(bg);
-
-        let client = MemoizeClientHandle::new(client);
-        let dns = match cfg_ate.dns_sec {
-            true => {
-                debug!("configured for DNSSec");
-                DnsClient::DnsSec(DnssecDnsHandle::new(client.clone()))
-            },
-            false => {
-                debug!("configured for plain DNS");
-                DnsClient::Dns(client)
-            }
-        };
+        let dns = DnsClient::connect(cfg_ate).await;
         let dns = Mutex::new(dns);
         
         Arc::new(
