3b01ad1fcbcf5421f60e125d5a62c75aad7c6535
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2021-09-12 11:50:31 +0200

    Added some debug information

--- ateweb/src/acme.rs
+++ ateweb/src/acme.rs
@@ -1,4 +1,5 @@
-
+#[allow(unused_imports, dead_code)]
+use tracing::{info, warn, debug, error, trace, instrument, span, Level};
 use rustls::ResolvesServerCert;
 use rustls::ClientHello;
 use rustls::sign::CertifiedKey;
@@ -19,7 +20,12 @@ impl Acme
 impl ResolvesServerCert
 for Acme
 {
-    fn resolve(&self, _client_hello: ClientHello) -> Option<CertifiedKey> {
+    fn resolve(&self, client_hello: ClientHello) -> Option<CertifiedKey> {
+        if let Some(from) = client_hello.server_name() {
+            trace!("tls_hello: from={:?}", from);
+        } else {
+            debug!("rejected connection (SNI was missing)");
+        }
         None
     }
 }
