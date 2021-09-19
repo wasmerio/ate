320d75e116605b9b90b5c7bc80d5b48ad19befdf
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2021-09-19 19:22:49 +0200

    Added another purpose to make the debug code easier to understand

--- lib/src/session/role_purpose.rs
+++ lib/src/session/role_purpose.rs
@@ -5,6 +5,7 @@ use serde::{Serialize, Deserialize, de::DeserializeOwned};
 pub enum AteRolePurpose
 {
     Owner,
+    Personal,
     Delegate,
     Contributor,
     Observer,
@@ -20,6 +21,7 @@ for AteRolePurpose
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             AteRolePurpose::Owner => write!(f, "owner"),
+            AteRolePurpose::Personal => write!(f, "personal"),
             AteRolePurpose::Delegate => write!(f, "delegate"),
             AteRolePurpose::Contributor => write!(f, "contributor"),
             AteRolePurpose::Observer => write!(f, "observer"),
@@ -39,6 +41,7 @@ for AteRolePurpose
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "owner" => Ok(AteRolePurpose::Owner),
+            "personal" => Ok(AteRolePurpose::Personal),
             "delegate" => Ok(AteRolePurpose::Delegate),
             "contributor" => Ok(AteRolePurpose::Contributor),
             "observer" => Ok(AteRolePurpose::Observer),
@@ -46,7 +49,7 @@ for AteRolePurpose
             "www" => Ok(AteRolePurpose::WebServer),
             "edge" => Ok(AteRolePurpose::EdgeCompute),
             a if a.starts_with("other-") && a.len() > 6 => Ok(AteRolePurpose::Other(a["other-".len()..].to_string())),
-            _ => Err("valid values are 'owner', 'delegate', 'contributor', 'observer' and 'other-'"),
+            _ => Err("valid values are 'owner', 'personal', 'delegate', 'contributor', 'observer', 'www', 'edge' and 'other-'"),
         }
     }
 }
