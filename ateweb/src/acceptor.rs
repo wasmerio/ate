92041c8dfcbaae6eef1b090563bb09792a7884fa
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2021-09-21 00:45:15 +0200

    Fixed the bug on the renewal - it was because the renewal time was set wrong

--- ateweb/src/acceptor.rs
+++ ateweb/src/acceptor.rs
@@ -145,7 +145,7 @@ impl HyperAcceptor
                 acme.touch_alpn(sni.to_string()).await?;
             } else {
                 trace!("connection attempt SNI: {}", sni);
-                acme.touch_web(sni.to_string(), chrono::Duration::days(100)).await?;
+                acme.touch_web(sni.to_string(), chrono::Duration::days(30)).await?;
             }
             break;
         }
