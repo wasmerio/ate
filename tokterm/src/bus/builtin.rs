1e98203b37c8e6ae6dc7001de9052dbaf72a8d74
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2021-11-25 21:15:59 +0100

    Quite a few fixes for the process RPC logic

---
+++ tokterm/src/bus/builtin.rs
@@ -0,0 +1,18 @@
+use std::sync::Arc;
+use std::any::type_name;
+use wasm_bus::backend;
+
+use super::*;
+
+pub fn builtin(_parent: Option<u32>, wapm: &str, topic: &str) -> Option<Arc<dyn Invokable>> {
+    match (wapm, topic) {
+        (wasm_bus::ws::WAPM_NAME, topic) if topic == type_name::<backend::ws::Connect>() => {
+            Some(CallbackInvokable::new(process_ws_connect))
+        }
+        _ => None
+    }
+}
+
+async fn process_ws_connect(_request: backend::ws::Connect) -> () {
+
+}
