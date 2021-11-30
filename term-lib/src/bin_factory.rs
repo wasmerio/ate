b95f92b9f020f2690e2c172388e829f97d6623a2
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2021-11-30 10:02:13 +0100

    Fixed a regression issue on the alias functionality

--- term-lib/src/bin_factory.rs
+++ term-lib/src/bin_factory.rs
@@ -41,23 +41,25 @@ impl BinaryPackage
 
 #[derive(Debug, Clone)]
 pub struct BinFactory {
+    pub alias: Arc<RwLock<HashMap<String, Option<String>>>>,
     pub cache: Arc<RwLock<HashMap<String, Option<BinaryPackage>>>>,
 }
 
 impl BinFactory {
     pub fn new() -> BinFactory {
         BinFactory {
+            alias: Arc::new(RwLock::new(HashMap::new())),
             cache: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
-    pub async fn get(&self, cmd: &str, mut stdout: Fd) -> Option<BinaryPackage> {
-        let mut cmd = cmd.to_string();
+    pub async fn get(&self, name: &str, mut stdout: Fd) -> Option<BinaryPackage> {
+        let mut name = name.to_string();
 
         // Fast path
         {
             let cache = self.cache.read().await;
-            if let Some(data) = cache.get(&cmd) {
+            if let Some(data) = cache.get(&name) {
                 return data.clone();
             }
         }
@@ -70,21 +72,59 @@ impl BinFactory {
         let mut cache = self.cache.write().await;
 
         // Check the cache
-        if let Some(data) = cache.get(&cmd) {
+        if let Some(data) = cache.get(&name) {
             stdout.write_clear_line().await;
             return data.clone();
         }
 
         // First just try to find it
-        if let Ok(data) = fetch_file(format!("/bin/{}.wasm", cmd).as_str()).await {
+        if let Ok(data) = fetch_file(format!("/bin/{}.wasm", name).as_str()).await {
             let data = BinaryPackage::new(Bytes::from(data));
-            cache.insert(cmd, Some(data.clone()));
+            cache.insert(name, Some(data.clone()));
             stdout.write_clear_line().await;
             return Some(data);
         }
 
         // NAK
-        cache.insert(cmd, None);
+        cache.insert(name, None);
+        stdout.write_clear_line().await;
+        return None;
+    }
+
+    pub async fn alias(&self, name: &str, mut stdout: Fd) -> Option<String> {
+        let mut name = name.to_string();
+
+        // Fast path
+        {
+            let cache = self.alias.read().await;
+            if let Some(data) = cache.get(&name) {
+                return data.clone();
+            }
+        }
+
+        // Tell the console we are fetching
+        stdout.write_clear_line().await;
+        let _ = stdout.write("Probing...".as_bytes()).await;
+        
+        // Slow path
+        let mut cache = self.alias.write().await;
+
+        // Check the cache
+        if let Some(data) = cache.get(&name) {
+            stdout.write_clear_line().await;
+            return data.clone();
+        }
+
+        // Try and find it via a fetch
+        if let Ok(data) = fetch_file(format!("/bin/{}.alias", name).as_str()).await {
+            let alias = String::from_utf8_lossy(&data[..]).to_string();
+            cache.insert(name, Some(alias.clone()));
+            stdout.write_clear_line().await;
+            return Some(alias);
+        }
+
+        // NAK
+        cache.insert(name, None);
         stdout.write_clear_line().await;
         return None;
     }
--- term-lib/src/eval/load_bin.rs
+++ term-lib/src/eval/load_bin.rs
@@ -23,35 +23,33 @@ pub async fn load_bin(
 {
     // Resolve any alias
     let mut already = HashSet::<String>::default();
-    let mut cmd = name.clone();
+    let mut name = name.clone();
+    info!("scanning for {}", format!("/bin/{}.alias", name));
     while let Ok(mut file) = ctx
         .root
         .new_open_options()
         .read(true)
-        .open(format!("/bin/{}.alias", cmd))
+        .open(format!("/bin/{}.alias", name))
     {
-        // Infinite loop check
-        if already.contains(&cmd) {
-            return None;
-        }
-        already.insert(cmd.clone());
+        if already.contains(&name) { break; }
+        already.insert(name.clone());
 
         let mut d = Vec::new();
         if let Ok(_) = file.read_to_end(&mut d) {
             let next = String::from_utf8_lossy(&d[..]).to_string();
-            debug!("binary alias '{}' found for {}", next, cmd);
-            cmd = next;
+            info!("binary alias '{}' found for {}", next, name);
+            name = next;
         } else {
             break;
         }
     }
 
     // Check if there is a file in the /bin and /wapm_packages/.bin folder
-    let mut file_checks = vec![format!("/bin/{}", cmd)];
-    if cmd.starts_with("/") {
-        file_checks.push(cmd.clone());
-    } else if cmd.starts_with("./") && cmd.len() > 2 {
-        file_checks.push(format!("{}{}", ctx.path, &cmd[2..]));
+    let mut file_checks = vec![format!("/bin/{}", name)];
+    if name.starts_with("/") {
+        file_checks.push(name.clone());
+    } else if name.starts_with("./") && name.len() > 2 {
+        file_checks.push(format!("{}{}", ctx.path, &name[2..]));
     }
     for file_check in file_checks {
         if let Ok(mut file) = ctx.root.new_open_options().read(true).open(file_check) {
@@ -63,7 +61,16 @@ pub async fn load_bin(
         }
     }
 
+    // Resolve some more alias possibilities using fetch commands
+    while let Some(next) = ctx.bins.alias(name.as_str(), stdio.stderr.clone()).await {
+        if already.contains(&name) { break; }
+        already.insert(name.clone());
+
+        info!("binary alias '{}' found for {}", next, name);
+        name = next;
+    }
+
     // Fetch the data asynchronously (from the web site)
-    ctx.bins.get(cmd.as_str(), stdio.stderr.clone())
+    ctx.bins.get(name.as_str(), stdio.stderr.clone())
         .await
 }
