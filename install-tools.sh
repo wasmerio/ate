0c3a04462c8e730affe5efbd88a01ed953ba54cd
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2021-12-25 08:18:35 +0100

    Fixed a dependency on the local changes to wasmer - switched to the pull request branch

--- install-tools.sh
+++ install-tools.sh
@@ -2,4 +2,12 @@
 pushd tokera >/dev/null
 cargo build --release
 popd >/dev/null
+pushd tokterm >/dev/null
+cargo build --release
+popd >/dev/null
+pushd atefs >/dev/null
+cargo build --release
+popd >/dev/null
 sudo cp -f target/release/tok /usr/bin/
+sudo cp -f target/release/tokterm /usr/bin/
+sudo cp -f target/release/atefs /usr/bin/
