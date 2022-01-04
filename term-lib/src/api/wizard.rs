985ae7de4a46801eeef5286eeffb1c12969e783e
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2022-01-04 19:24:06 +0100

    Added missing files

---
+++ term-lib/src/api/wizard.rs
@@ -0,0 +1,28 @@
+pub struct WizardPrompt
+{
+    pub prompt: String,
+    pub echo: bool,
+}
+
+pub enum WizardAction
+{
+    Challenge {
+        name: String,
+        instructions: String,
+        prompts: Vec<WizardPrompt>,
+    },
+    Shell,
+    Terminate {
+        with_message: Option<String>,
+    }
+}
+
+/// The wizard ABI allows for actions to be taken before the terminal
+/// is actually fully running
+#[async_trait::async_trait]
+pub trait WizardAbi
+{
+    async fn process(&mut self, responses: Vec<String>) -> WizardAction;
+
+    fn token(&self) -> Option<String>;
+}
