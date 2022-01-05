b553701a3ffa23bdfa009cb5087c1cdf4332a4d5
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2022-01-05 19:23:15 +0100

    Can now easily embed any files into the bootstrap process

--- term-lib/src/fs/utils.rs
+++ term-lib/src/fs/utils.rs
@@ -6,52 +6,39 @@ use tracing::{debug, error, info, trace, warn};
 
 use super::*;
 
-static BIN_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/bin");
+static STATIC_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/static");
 
 pub fn create_root_fs() -> UnionFileSystem {
     let mut mounts = UnionFileSystem::new();
     mounts.mount("root", Path::new("/"), Box::new(TmpFileSystem::default()));
-    mounts.create_dir(&Path::new("/bin")).unwrap();
-    mounts.create_dir(&Path::new("/dev")).unwrap();
-    mounts.create_dir(&Path::new("/etc")).unwrap();
-    mounts.create_dir(&Path::new("/tmp")).unwrap();
-    mounts.create_dir(&Path::new("/.private")).unwrap();
-
-    append_static_files(&mut mounts);
-
-    let mut os_release = mounts
-        .new_open_options()
-        .create_new(true)
-        .write(true)
-        .open("/etc/os-release")
-        .unwrap();
-
-    os_release
-        .write_all(
-            r#"PRETTY_NAME="Tokera WebAssembly Shell 1"
-NAME="Tokera/Wasm"
-VERSION_ID="1"
-VERSION="1"
-VERSION_CODENAME=tok
-ID=debian
-HOME_URL="https://www.tokera.com/"#
-                .as_bytes(),
-        )
-        .unwrap();
-
+    append_static_dir(&mut mounts, &STATIC_DIR);
     mounts
 }
 
-pub fn append_static_files(fs: &mut UnionFileSystem) {
-    for file in BIN_DIR.files() {
+pub fn append_static_dir(fs: &mut UnionFileSystem, dir: &Dir) {
+    for dir in dir.dirs() {
+        if let Some(path) = dir.path().to_str() {
+            let path = format!("/{}", path);
+            let path = Path::new(path.as_str());
+            if fs.create_dir(path).is_ok() {            
+                append_static_dir(fs, dir);
+            }
+        }
+    }
+    for file in dir.files() {
+        if let Some(filename) = file.path().file_name() {
+            if filename.to_str() == Some(".marker") {
+                continue;
+            }
+        }
         if let Some(path) = file.path().to_str() {
-            let target = format!("/bin/{}", path);
-            
+            let path = format!("/{}", path);
+
             let mut bin = fs
                 .new_open_options()
                 .create_new(true)
                 .write(true)
-                .open(target.as_str())
+                .open(path.as_str())
                 .unwrap();
 
             bin.write_all(file.contents()).unwrap();
