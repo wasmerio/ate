dfb750d8d04d0208c256771530430f751bf76536
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2022-04-10 20:56:57 +0200

    Checking in my code before the laptop blows up

--- atenet/src/gateway.rs
+++ atenet/src/gateway.rs
@@ -1,8 +1,13 @@
 use std::sync::Arc;
 use std::sync::Weak;
 use std::sync::RwLock;
-use std::collections::HashMap;
-use tokera::model::IpCidr;
+use smoltcp::wire::EthernetAddress;
+use smoltcp::wire::EthernetFrame;
+use smoltcp::wire::Ipv4Packet;
+use smoltcp::wire::Ipv6Packet;
+use smoltcp::wire::EthernetProtocol;
+use smoltcp::wire::IpCidr;
+use smoltcp::wire::IpAddress;
 
 use super::switch::Switch;
 use super::factory::SwitchFactory;
@@ -21,7 +26,7 @@ pub struct Route
 #[derive(Debug, Default)]
 pub struct GatewayState
 {
-    routes: HashMap<IpCidr, Route>
+    routes: Vec<Route>,
 }
 
 #[allow(dead_code)]
@@ -35,6 +40,8 @@ pub struct Gateway
 
 impl Gateway
 {
+    pub const MAC: EthernetAddress = EthernetAddress([6u8, 0u8, 0u8, 0u8, 0u8, 1u8]);
+    
     pub fn new(id: u128, factory: &Arc<SwitchFactory>) -> Gateway {
         Gateway {
             id,
@@ -43,8 +50,51 @@ impl Gateway
         }
     }
 
-    pub fn process_outbound(&self, _pck: &[u8]) {
+    pub fn process_outbound(&self, mut pck: Vec<u8>)
+    {
+        // Packets going to another switch that we have a routing table entry
+        // to need to have their MAC address modified then transmitted
+        let state = self.state.read().unwrap();
+        if let Ok(mut frame_mac) = EthernetFrame::new_checked(&mut pck[..]) {
+            let dst_ip = match frame_mac.ethertype() {
+                EthernetProtocol::Ipv4 => {
+                    if let Ok(frame_ip) = Ipv4Packet::new_checked(frame_mac.payload_mut()) {
+                        let dst_ip = IpAddress::Ipv4(frame_ip.dst_addr());
+                        drop(frame_ip);
+                        Some(dst_ip)
+                    } else {
+                        None
+                    }
+                }
+                EthernetProtocol::Ipv6 => {
+                    if let Ok(frame_ip) = Ipv6Packet::new_checked(frame_mac.payload_mut()) {
+                        let dst_ip = IpAddress::Ipv6(frame_ip.dst_addr());
+                        drop(frame_ip);
+                        Some(dst_ip)
+                    } else {
+                        None
+                    }
+                }
+                _ => None
+            };
 
+            if let Some(dst_ip) = dst_ip {
+                for route in state.routes.iter() {
+                    if route.cidr.contains_addr(&dst_ip) {
+                        if let Some(switch) = route.switch.upgrade() {
+                            if let Some(dst_mac) = switch.lookup_ip(&dst_ip) {
+                                frame_mac.set_src_addr(Self::MAC);
+                                frame_mac.set_dst_addr(dst_mac);
+                                drop(frame_mac);
+                                
+                                switch.unicast(&Self::MAC, &dst_mac, pck, true);
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     pub fn process_inbound(&self, _pck: &[u8]) {
--- atenet/src/switch.rs
+++ atenet/src/switch.rs
@@ -3,14 +3,24 @@ use std::collections::HashMap;
 use std::sync::Arc;
 use std::ops::*;
 use std::sync::RwLockReadGuard;
+use std::time::Duration;
 use ate_files::prelude::FileAccessor;
 use tokio::sync::mpsc;
 use smoltcp::wire::EthernetAddress;
 use smoltcp::wire::EthernetFrame;
+use smoltcp::wire::EthernetProtocol;
+use smoltcp::wire::ArpPacket;
+use smoltcp::wire::ArpHardware;
+use smoltcp::wire::IpAddress;
+use smoltcp::wire::Ipv4Address;
+use smoltcp::wire::Ipv4Packet;
+use smoltcp::wire::Ipv6Address;
+use smoltcp::wire::Ipv6Packet;
 use derivative::*;
 use tokio::sync::RwLock;
 use std::sync::RwLock as StdRwLock;
 use ate::prelude::*;
+use ttl_cache::TtlCache;
 use tokera::model::MeshNode;
 use tokera::model::HardwareAddress;
 use tokera::model::ServiceInstance;
@@ -42,6 +52,14 @@ pub struct SwitchPort {
 pub struct DataPlane {
     pub(crate) ports: HashMap<EthernetAddress, Destination>,
     pub(crate) peers: HashMap<PrimaryKey, IpAddr>,
+    #[derivative(Debug = "ignore")]
+    pub(crate) mac4: TtlCache<EthernetAddress, Ipv4Address>,
+    #[derivative(Debug = "ignore")]
+    pub(crate) ip4: TtlCache<Ipv4Address, EthernetAddress>,
+    #[derivative(Debug = "ignore")]
+    pub(crate) mac6: TtlCache<EthernetAddress, Ipv6Address>,
+    #[derivative(Debug = "ignore")]
+    pub(crate) ip6: TtlCache<Ipv6Address, EthernetAddress>,
 }
 
 #[derive(Derivative)]
@@ -69,6 +87,9 @@ pub struct Switch
 
 impl Switch
 {
+    pub const MAC_SNOOP_MAX: usize = u16::MAX as usize;
+    pub const MAC_SNOOP_TTL: Duration = Duration::from_secs(14400); // 4 hours (CISCO default)
+
     pub async fn new(accessor: Arc<FileAccessor>, udp: UdpPeer, gateway: Arc<Gateway>) -> Result<Arc<Switch>, AteError> {
         let (inst, bus, me_node) = {
             let chain_dio = accessor.dio.clone().as_mut().await;
@@ -119,6 +140,10 @@ impl Switch
                 DataPlane {
                     ports: Default::default(),
                     peers: Default::default(),
+                    mac4: TtlCache::new(Self::MAC_SNOOP_MAX),
+                    ip4: TtlCache::new(Self::MAC_SNOOP_MAX),
+                    mac6: TtlCache::new(Self::MAC_SNOOP_MAX),
+                    ip6: TtlCache::new(Self::MAC_SNOOP_MAX),
                 }
             ),
             control_plane: RwLock::new(
@@ -174,32 +199,45 @@ impl Switch
 
     pub fn broadcast(&self, src: &EthernetAddress, pck: Vec<u8>) {
         let state = self.data_plane.read().unwrap();
-        self.__broadcast(&state, src, pck);
+        self.__broadcast(&state, src, &pck[..]);
     }
     
-    fn __broadcast(&self, state: &RwLockReadGuard<DataPlane>, src: &EthernetAddress, pck: Vec<u8>) {
+    fn __broadcast(&self, state: &RwLockReadGuard<DataPlane>, src: &EthernetAddress, pck: &[u8]) {
         for (mac, dst) in state.ports.iter() {
             if let Destination::Local(port) = dst {
                 if src != mac {
-                    let _ = port.tx.blocking_send(pck.clone());
+                    let _ = port.tx.blocking_send(pck.to_vec());
                 }
             }
         }
 
         // Encrypt and sign the packet before we send it
-        let pck = self.encrypt_packet(&pck[..]);
+        let pck = self.encrypt_packet(pck);
         for peer in state.peers.values() {
             self.udp.send(&pck[..], peer.clone());
         }
     }
 
-    pub fn unicast(&self, src: &EthernetAddress, dst_mac: &EthernetAddress, pck: Vec<u8>, allow_forward: bool) {
+    pub fn unicast(&self, src: &EthernetAddress, dst_mac: &EthernetAddress, pck: Vec<u8>, allow_forward: bool)
+    {
+        // If the packet is going to the default gateway then we
+        // should pass it to our dateway engine to process instead
+        if dst_mac == &Gateway::MAC {
+            self.gateway.process_outbound(pck);
+            return;
+        }
 
+        // Next we lookup if this destination address is known either
+        // on this switch node or another one
         let state = self.data_plane.read().unwrap();
         if let Some(dst) = state.ports.get(&dst_mac) {
             match dst {
                 Destination::Local(port) => {
-                    let _ = port.tx.blocking_send(pck);    
+                    let tx = port.tx.clone();
+
+                    self.snoop(state, &pck);
+                    let _ = tx.blocking_send(pck);
+                    return;
                 },
                 Destination::PeerSwitch(peer) => {
                     if allow_forward {
@@ -208,9 +246,97 @@ impl Switch
                     }
                 }
             }
-        } else if allow_forward {
-            self.__broadcast(&state, src, pck);
         }
+
+        // Otherwise we broadcast it to all the other nodes as it
+        // could be that we just dont know about it yet or it could
+        // be that its multicast/broadcast traffic.
+        else if allow_forward {
+            self.__broadcast(&state, src, &pck);
+        }
+
+        // Snoop the packet
+        self.snoop(state, &pck);
+    }
+
+    pub fn snoop(&self, state: RwLockReadGuard<DataPlane>, pck: &[u8]) {
+        if let Ok(frame_mac) = EthernetFrame::new_checked(&pck[..]) {
+            let mac = frame_mac.src_addr();
+            if mac.is_unicast() {
+                match frame_mac.ethertype() {
+                    EthernetProtocol::Ipv4 => {
+                        if let Ok(frame_ip) = Ipv4Packet::new_checked(frame_mac.payload()) {
+                            let ip = frame_ip.src_addr();
+
+                            let update_mac4 = state.mac4.contains_key(&mac);
+                            let update_ip4 = state.ip4.contains_key(&ip);
+                            drop(state);
+
+                            if update_mac4 || update_ip4 {
+                                let mut state = self.data_plane.write().unwrap();
+                                state.mac4.insert(mac, ip, Self::MAC_SNOOP_TTL);
+                                state.ip4.insert(ip, mac, Self::MAC_SNOOP_TTL);
+                            }
+                            return;
+                        }
+                    },
+                    EthernetProtocol::Ipv6 => {
+                        if let Ok(frame_ip) = Ipv6Packet::new_checked(frame_mac.payload()) {
+                            let ip = frame_ip.src_addr();
+
+                            let update_mac6 = state.mac6.contains_key(&mac);
+                            let update_ip6 = state.ip6.contains_key(&ip);
+                            drop(state);
+
+                            if update_mac6 || update_ip6 {
+                                let mut state = self.data_plane.write().unwrap();
+                                state.mac6.insert(mac, ip, Self::MAC_SNOOP_TTL);
+                                state.ip6.insert(ip, mac, Self::MAC_SNOOP_TTL);
+                            }
+                            return;
+                        }
+                    },
+                    EthernetProtocol::Arp => {
+                        if let Ok(frame_arp) = ArpPacket::new_checked(frame_mac.payload()) {
+                            if frame_arp.hardware_type() == ArpHardware::Ethernet &&
+                               frame_arp.protocol_type() == EthernetProtocol::Ipv4
+                            {
+                                let mac = EthernetAddress::from_bytes(frame_arp.source_hardware_addr());
+                                let ip = Ipv4Address::from_bytes(frame_arp.source_protocol_addr());
+
+                                drop(state);
+                                let mut state = self.data_plane.write().unwrap();
+                                state.mac4.insert(mac, ip, Self::MAC_SNOOP_TTL);
+                                state.ip4.insert(ip, mac, Self::MAC_SNOOP_TTL);
+                                return;
+                            }
+                        }
+                    },
+                    _ => { }
+                }
+            }
+        }
+    }
+
+    pub fn lookup_ip(&self, ip: &IpAddress) -> Option<EthernetAddress>
+    {
+        match ip {
+            IpAddress::Ipv4(ip) => self.lookup_ipv4(ip),
+            IpAddress::Ipv6(ip) => self.lookup_ipv6(ip),
+            _ => None
+        }
+    }
+
+    pub fn lookup_ipv4(&self, ip: &Ipv4Address) -> Option<EthernetAddress>
+    {
+        let state = self.data_plane.read().unwrap();
+        state.ip4.get(ip).map(|mac| mac.clone())
+    }
+
+    pub fn lookup_ipv6(&self, ip: &Ipv6Address) -> Option<EthernetAddress>
+    {
+        let state = self.data_plane.read().unwrap();
+        state.ip6.get(ip).map(|mac| mac.clone())
     }
 
     pub fn encrypt_packet(&self, pck: &[u8]) -> Vec<u8> {
